6e9daa6233af2399142d459298dd96c2d17336db
yungo1846 <yungo1468@naver.com>
2021-09-25 17:48:08 +0900

    feat: dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,37 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home({ postData }) {
-  const { id, title, body } = postData;
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          <li className={utilStyles.listItem} key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {body}
-          </li>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
         </ul>
       </section>
     </Layout>
   );
 }
 
-export async function getServerSideProps(context) {
-  const id = context.query.id;
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const postData = await response.json();
-
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      postData,
+      allPostsData,
     },
   };
 }
